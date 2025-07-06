6636655ec55c9cce797400c76870849eeacbc065
giogiamon <giogia.mon@gmail.com>
2025-07-06 19:44:56 +0200

    feat: complete Day 006 – Number Guessing Game using while loops and flags

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -16,6 +16,9 @@ def my_function():
 #     turn_left()
 #     turn_left()
 
+
+
+
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 # Reeborg's world
 # def turn_right():
