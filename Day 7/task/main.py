57ebc9bb9ea4f53bd982bcd08f36d25f01b3da63
giogiamon <giogia.mon@gmail.com>
2025-07-07 16:38:29 +0200

    feat: complete day 7 Hangman Game Stage 3 of Hangman - modularized word list and ASCII art, added guess history and feedback

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -2,26 +2,67 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-print(f"You have to guess {chosen_word}")
+print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 placeholder = ""
-for n in range(len(chosen_word)):
-    placeholder=placeholder+"_"
-print(f"You have to guess {placeholder}")
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
-guess = input("Guess a letter: ").lower()
-
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+# TODO-1: - Use a while loop to let the user guess again.
+# write the while loop
+# initialize the display word
 display = ""
-
-for letter in chosen_word:
-    if letter == guess:
-        # print("Right")
-        display=display+letter
+# initialize the attempts and max attempts
+attempts = 0
+max_attempts = 7
+# initialize the guessed letters - correct ones
+list_of_guessed = []
+# the while loop
+while "_" in placeholder and attempts < max_attempts:
+    guess = input("Guess a letter: ").lower()
+    # check if the attempt is valid
+    if not guess.isalpha() or len(guess) != 1:
+        print("Please enter a single letter.")
+        continue
+    #  check if users already catch the letter
+    if guess in list_of_guessed:
+        print(f"You already guessed '{guess}'. Try another letter.")
+        continue
+    # initialize the guessed attempt
+    guessed = False
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+    # check if guessed or not
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            guessed = True
+            #  add only the first time!!
+            if guess not in list_of_guessed:
+                list_of_guessed.append(guess)
+    # build the word to display
+    for letter in chosen_word:
+        if letter in list_of_guessed:
+            display += letter
+        else:
+            display += "_"
+    # Add an attempt
+    attempts += 1
+    if guessed:
+        print(f"Good Try! Current word {display}")
+        placeholder = display
     else:
-        # print("Wrong")
-        display = display + "_"
+        print(f"Wrong guess. Attempts: {attempts}/{max_attempts}\n Current word {display}")
+    # initialize the on-going print word variable
+    display = ""
+    print(f"Attempt number {attempts}, the word: {placeholder}")
 
-print(f"Now the word is: {display}")
+# final print out: notify the user the result
+if "_" not in placeholder:
+    print("🎉 You won!")
+else:
+    print("💀 You lost!")
+
+
+
 
