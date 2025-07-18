270fff9e46620d87b3562200835aa3f537953ec3
giogiamon <giogia.mon@gmail.com>
2025-07-18 19:06:01 +0200

    Add Day 009: Dictionaries and nested data structures. Includes two versions of Blind Auction (basic and refactored)

--- Day 9/Blind Auction Project/main_v2.py
+++ Day 9/Blind Auction Project/main_v2.py
@@ -15,6 +15,23 @@ def find_highest_bid(bidders):
             winner = bid
     print(f"The winner is {winner} with a bid of: ${max_bid}")
 
+# function to check if the bid is valid
+def is_valid_bid():
+    while True:
+        bid_input = input("What is your bid?: $")
+        if bid_input.replace('.','',1).isdigit():
+            return float(bid_input)
+        else:
+            print("Please enter a valid number.")
+# function to check yes or no
+def check_yes_or_no(answer):
+    while True:
+        response = input(answer).lower()
+        if response in ["yes", "no"]:
+            return response
+        else:
+            print("Please type only 'yes' or 'no'")
+
 # always ask for the first bidder
 bid_continue = True
 # initialize the bidders dictionary
@@ -22,12 +39,13 @@ bidders = {}
 while bid_continue:
     # ask name and bid
     name = input("What is your name?: ")
-    # convert the input in float
-    bid = float(input("What is your bid?: $"))
+    # Check the bid is correct
+    # bid = input("What is your bid?: $")
+    bid = is_valid_bid()
     # add to the dictionary
     bidders[name] = bid
     # Check if there are any other bidders
-    add_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    add_bidders = check_yes_or_no("Are there any other bidders? Type 'yes' or 'no'.\n")
     if add_bidders == "yes":
         # clear the screen
         print("\n" * 20)
